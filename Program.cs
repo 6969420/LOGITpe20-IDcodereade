c83ac96df3f53b3e3e4adf89967f94a5112e3ca8
Opilane TTHK <opilane@tthk.ee>
2020-10-21 15:59:30 +0300

    something

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CVVValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
